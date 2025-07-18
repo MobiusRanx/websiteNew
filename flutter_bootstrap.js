d36b4d779093824af436332b6c983dd54dbd3e29
MobiusRanx <d.kohne15@gmail.com>
2025-07-18 16:10:31 +0200

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1996028498"
+    serviceWorkerVersion: "4120093957"
   }
 });
